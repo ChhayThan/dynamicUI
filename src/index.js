d23559ef9b93a77eeb0861069cfd2262b35de330
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-05-16 22:10:38 -0400

    added images | will continue to work on image carousel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./style.css";
+import CliffSidePic from "../images/CliffSide.jpeg";
+import EmptySubway from "../images/EmptySubway.jpeg";
+import NatureView from "../images/NatureView.avif";
+import TallBuilding from "../images/TallBuilding.jpg";
+
+const images = [CliffSidePic, EmptySubway, NatureView, TallBuilding];
 
 const dropDownBtn = document.querySelector("button.btn.dropDown");
 dropDownBtn.addEventListener("click", () => {
@@ -11,4 +17,15 @@ dropDownBtn.addEventListener("click", () => {
     dropDownOptions.classList.add("active");
     dropDownBtn.innerText = "BackUp👆";
   }
+});
+
+images.forEach((image) => {
+  const imageList = document.querySelector("ul.image_list");
+  const li = document.createElement("li");
+  li.classList.add("slide");
+  const img = document.createElement("img");
+  img.src = image;
+  img.alt = `${image} picture`;
+  li.appendChild(img);
+  imageList.appendChild(li);
 });
