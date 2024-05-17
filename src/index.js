9e910a165893cae640ce08561a32d3f253289aca
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-05-16 20:09:07 -0400

    Created dropDown button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import "./style.css";
 
-console.log("Testing");
+const dropDownBtn = document.querySelector("button.btn.dropDown");
+dropDownBtn.addEventListener("click", () => {
+  const dropDownOptions = document.querySelector("div.dropDownOptions");
+
+  if (dropDownOptions.classList.contains("active")) {
+    dropDownOptions.classList.remove("active");
+    dropDownBtn.innerText = "DropDown👇";
+  } else {
+    dropDownOptions.classList.add("active");
+    dropDownBtn.innerText = "BackUp👆";
+  }
+});
